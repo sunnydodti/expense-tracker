7111185a51938d00ac57849297a56b7bcf83b3cd
sunnydodti <sunnydodti@gmail.com>
2024-04-14 03:25:11 +0530

    move import to settings

--- expense_tracker/lib/ui/drawer/home_drawer.dart
+++ expense_tracker/lib/ui/drawer/home_drawer.dart
@@ -1,18 +1,13 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:share/share.dart';
 
-import '../../data/constants/response_constants.dart';
 import '../../models/export_result.dart';
-import '../../models/import_result.dart';
 import '../../providers/expense_provider.dart';
 import '../../service/category_service.dart';
 import '../../service/expense_service.dart';
 import '../../service/export_service.dart';
-import '../../service/import_service.dart';
-import '../../service/path_service.dart';
 import '../../service/tag_service.dart';
 import '../notifications/snackbar_service.dart';
 import '../screens/category_screen.dart';
@@ -30,24 +25,10 @@ class HomeDrawerState extends State<HomeDrawer> {
   final Logger _logger = Logger(printer: SimplePrinter(), level: Level.info);
 
   bool _isDeleteDialogVisible = false;
-  bool _isImportDialogVisible = false;
-
-  String _exportFilePath = '';
-  String _selectedFileName = 'Please select a file';
-  String _selectedFilePath =
-      '/data/user/0/com.sunnydodti.expense_tracker/cache/file_picker/expense_tracker_export.zip';
 
   @override
   void initState() {
     super.initState();
-    _getExportFilePath();
-  }
-
-  Future<void> _getExportFilePath() async {
-    final filePath = await PathService.fileExportPath;
-    setState(() {
-      _exportFilePath = filePath;
-    });
   }
 
   @override
@@ -73,14 +54,6 @@ class HomeDrawerState extends State<HomeDrawer> {
               },
             ),
             ListTile(
-              title: const Text('Import'),
-              onTap: () {
-                setState(() {
-                  _isImportDialogVisible = true;
-                });
-              },
-            ),
-            ListTile(
               title: const Text('Export'),
               onTap: () => _exportExpenses(context),
             ),
@@ -97,36 +70,26 @@ class HomeDrawerState extends State<HomeDrawer> {
               onTap: () => _navigateToTagScreen(context),
             ),
             if (_isDeleteDialogVisible) _buildDeleteConfirmationDialog(context),
-            if (_isImportDialogVisible) _buildImportDialog(context),
           ],
         ),
       ),
     );
   }
 
-  void _navigateToSettingsScreen(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const SettingsScreen(),
-      ),
-    );
-  }
+  void _navigateToSettingsScreen(BuildContext context) =>
+      _navigateToScreen(context, const SettingsScreen());
 
-  _navigateToCategoryScreen(BuildContext context) async {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const CategoryScreen(),
-      ),
-    );
-  }
+  void _navigateToCategoryScreen(BuildContext context) =>
+      _navigateToScreen(context, const CategoryScreen());
 
-  _navigateToTagScreen(BuildContext context) async {
+  void _navigateToTagScreen(BuildContext context) =>
+      _navigateToScreen(context, const TagScreen());
+
+  void _navigateToScreen(BuildContext context, Widget screenWidget) {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => const TagScreen(),
+        builder: (context) => screenWidget,
       ),
     );
   }
@@ -154,70 +117,6 @@ class HomeDrawerState extends State<HomeDrawer> {
         ),
       ],
     );
-  }
-
-  Widget _buildImportDialog(BuildContext context) {
-    return AlertDialog(
-      title: const Text("Import File"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Export Path:\n$_exportFilePath'),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: _selectFile,
-            child: const Text('Select File'),
-          ),
-          const SizedBox(height: 8),
-          _showSelectedFileName(),
-        ],
-      ),
-      actions: <Widget>[
-        TextButton(
-          child: const Text("Cancel"),
-          onPressed: () {
-            setState(() {
-              _isImportDialogVisible = false;
-            });
-          },
-        ),
-        TextButton(
-          child: const Text("Import"),
-          onPressed: () {
-            if (_selectedFileName != '') {
-              _importExpenses();
-            }
-          },
-        ),
-      ],
-    );
-  }
-
-  void _importExpenses() async {
-    setState(() {
-      _isImportDialogVisible = false;
-    });
-    SnackBarService.showSnackBarWithContext(context, "imported started",
-        duration: 1);
-    ImportService importService = ImportService();
-    importService
-        .importFile(_selectedFilePath, _refreshExpenses)
-        .then((ImportResult result) {
-      _logger.i(result.toString());
-      Navigator.pop(context);
-      if (result.result) {
-        _refreshExpenses();
-        String message = "Import complete"
-            "\nExpenses:    ${result.expense.successCount}/${result.expense.total}"
-            "\nCategories:  ${result.category.successCount}/${result.category.total}"
-            "\nTags:             ${result.tag.successCount}/${result.tag.total}\n";
-        SnackBarService.showSuccessSnackBarWithContext(context, message,
-            duration: 5);
-      } else {
-        SnackBarService.showErrorSnackBarWithContext(context, result.message);
-      }
-    });
   }
 
   void _exportExpenses(BuildContext context) async {
@@ -266,21 +165,6 @@ class HomeDrawerState extends State<HomeDrawer> {
         ],
       ),
     );
-  }
-
-  Text _showSelectedFileName() {
-    if (_selectedFileName != "") return Text(_selectedFileName);
-    return const Text("");
-  }
-
-  Future<void> _selectFile() async {
-    PlatformFile? file = await ImportService.getJsonFileFromUser();
-    if (file != null) {
-      setState(() {
-        _selectedFileName = file.name;
-        _selectedFilePath = file.path!;
-      });
-    }
   }
 
   Future<void> _handleDelete(BuildContext context) async {
--- expense_tracker/lib/ui/screens/settings_screen.dart
+++ expense_tracker/lib/ui/screens/settings_screen.dart
@@ -1,9 +1,29 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+import '../../models/import_result.dart';
+import '../../providers/expense_provider.dart';
+import '../../service/import_service.dart';
+import '../notifications/snackbar_service.dart';
+
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   final String title = "Settings";
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final Logger _logger = Logger(printer: SimplePrinter(), level: Level.info);
+  String exportFilePath = '';
+
+  String _selectedFileName = 'Please select a file';
+  String _selectedFilePath =
+      '/data/user/0/com.sunnydodti.expense_tracker/cache/file_picker/expense_tracker_export.zip';
 
   Future<void> _refreshSettings(BuildContext context) async {
     // final provider = Provider.of<CategoryProvider>(context, listen: false);
@@ -27,16 +47,23 @@ class SettingsScreen extends StatelessWidget {
             appBar: AppBar(
               leading: SafeArea(
                   child: BackButton(
-                    onPressed: () => navigateBack(context, false),
-                  )),
+                onPressed: () => navigateBack(context, false),
+              )),
               centerTitle: true,
-              title: Text(title, textScaleFactor: 0.9),
+              title: Text(widget.title, textScaleFactor: 0.9),
               backgroundColor: Colors.black,
             ),
             body: Column(
-              children: const [
+              children: [
                 Expanded(
-                  child: Placeholder(),
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: const Text('Import'),
+                        onTap: () => _showImportDialog(context),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
@@ -44,5 +71,90 @@ class SettingsScreen extends StatelessWidget {
         }
       },
     );
+  }
+
+  Future<void> _showImportDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Import File"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Export Path:\n$exportFilePath'),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _selectFile,
+                child: const Text('Select File'),
+              ),
+              const SizedBox(height: 8),
+              _showSelectedFileName(),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text("Import"),
+              onPressed: () {
+                if (_selectedFileName != '') {
+                  _importExpenses();
+                }
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _selectFile() async {
+    PlatformFile? file = await ImportService.getJsonFileFromUser();
+    if (file != null) {
+      setState(() {
+        _selectedFileName = file.name;
+        _selectedFilePath = file.path!;
+      });
+    }
+  }
+
+  Text _showSelectedFileName() {
+    if (_selectedFileName != "") return Text(_selectedFileName);
+    return const Text("");
+  }
+
+  void _importExpenses() async {
+    SnackBarService.showSnackBarWithContext(context, "imported started",
+        duration: 1);
+    ImportService importService = ImportService();
+    importService
+        .importFile(_selectedFilePath, _refreshExpenses)
+        .then((ImportResult result) {
+      _logger.i(result.toString());
+      if (result.result) {
+        _refreshExpenses();
+        String message = "Import complete"
+            "\nExpenses:    ${result.expense.successCount}/${result.expense.total}"
+            "\nCategories:  ${result.category.successCount}/${result.category.total}"
+            "\nTags:             ${result.tag.successCount}/${result.tag.total}\n";
+        SnackBarService.showSuccessSnackBarWithContext(context, message,
+            duration: 5);
+      } else {
+        SnackBarService.showErrorSnackBarWithContext(context, result.message);
+      }
+    });
+  }
+
+  _refreshExpenses() {
+    final expenseProvider =
+        Provider.of<ExpenseProvider>(context, listen: false);
+    expenseProvider.refreshExpenses();
   }
 }
