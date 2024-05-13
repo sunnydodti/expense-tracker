bc51668efa14c732991ade1b867636cc209701d1
sunnydodti <sunnydodti@gmail.com>
2024-05-13 14:58:25 +0530

    bottomsheet - swipe to dismiss & deoration

--- expense_tracker/lib/ui/bottom_sheets/expense_bottom_sheet.dart
+++ expense_tracker/lib/ui/bottom_sheets/expense_bottom_sheet.dart
@@ -11,32 +11,83 @@ class ExpenseBottomSheet {
     showModalBottomSheet<void>(
       context: context,
       isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(50))),
       builder: (BuildContext context) {
-        return Container(
-          // height: 200,
-          color: Colors.grey.shade900,
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              ListTile(
-                title: const Text("View"),
-                trailing: const Icon(Icons.notes_outlined),
-                onTap: () => handleCallBack(context, viewCallBack),
-              ),
-              ListTile(
-                title: const Text("Edit"),
-                trailing: const Icon(Icons.edit_outlined),
-                onTap: () => handleCallBack(context, editCallBack),
-              ),
-              ListTile(
-                title: const Text("Delete"),
-                trailing: const Icon(Icons.delete_outline),
-                onTap: () => handleCallBack(context, deleteCallBack, pop: true),
-              ),
-            ],
-          ),
+        return _buildExpenseBottomSheetContent(
+          context,
+          expense,
+          viewCallBack,
+          editCallBack,
+          deleteCallBack,
         );
       },
+    );
+  }
+
+  static Container _buildExpenseBottomSheetContent(
+      BuildContext context,
+      Expense expense,
+      VoidCallback viewCallBack,
+      VoidCallback editCallBack,
+      VoidCallback deleteCallBack) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+        color: Colors.grey.shade900,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildDecoration(),
+          Text(expense.title),
+          const Divider(),
+          _buildOptionList(context, viewCallBack, editCallBack, deleteCallBack),
+        ],
+      ),
+    );
+  }
+
+  static Padding _buildDecoration() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        height: 5,
+        width: 50,
+        decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+      ),
+    );
+  }
+
+  static ListView _buildOptionList(
+      BuildContext context,
+      VoidCallback viewCallBack,
+      VoidCallback editCallBack,
+      VoidCallback deleteCallBack) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        ListTile(
+          iconColor: Colors.green.shade800,
+          title: const Text("View"),
+          trailing: const Icon(Icons.notes_outlined),
+          onTap: () => handleCallBack(context, viewCallBack),
+        ),
+        ListTile(
+          iconColor: Colors.orange.shade800,
+          title: const Text("Edit"),
+          trailing: const Icon(Icons.edit_outlined),
+          onTap: () => handleCallBack(context, editCallBack),
+        ),
+        ListTile(
+          iconColor: Colors.red.shade800,
+          title: const Text("Delete"),
+          trailing: const Icon(Icons.delete_outline),
+          onTap: () => handleCallBack(context, deleteCallBack, pop: true),
+        ),
+      ],
     );
   }
 
