74e66f25f79bb5327315c95a752c7c414339724f
sunnydodti <sunnydodti@gmail.com>
2024-05-30 23:58:32 +0530

    add scaffolds for different screen sizes

---
+++ expense_tracker/lib/responsive/responsive_layout.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScaffold;
+  final Widget tabletScaffold;
+  final Widget desktopScaffold;
+
+  const ResponsiveLayout(
+      {super.key,
+      required this.mobileScaffold,
+      required this.tabletScaffold,
+      required this.desktopScaffold});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      double width = constraints.maxWidth;
+      if (width <= 500) return mobileScaffold;
+      if (width <= 1100) return tabletScaffold;
+      return desktopScaffold;
+    });
+  }
+}
