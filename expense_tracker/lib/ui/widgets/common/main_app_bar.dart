1a1cfa94db7d2acb912df2caa4d2a1c1af4758f8
sunnydodti <sunnydodti@gmail.com>
2024-06-05 00:19:42 +0530

    add options to resopnsive app bar

--- expense_tracker/lib/ui/widgets/common/main_app_bar.dart
+++ expense_tracker/lib/ui/widgets/common/main_app_bar.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../data/helpers/debug_helper.dart';
+import '../../../data/helpers/navigation_helper.dart';
+import '../../../providers/expense_provider.dart';
+import '../../../service/expense_service.dart';
+import '../../dialogs/common/message_dialog.dart';
+import '../../screens/settings/settings_screen.dart';
 
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
-  final VoidCallback? populateExpense;
-  final VoidCallback? navigateToScreen;
-  final VoidCallback? handleProfile;
+
+  static final Future<ExpenseService> _expenseService = ExpenseService.create();
 
   const MainAppBar({
     super.key,
     this.title = "Expense Tracker",
-    this.populateExpense,
-    this.navigateToScreen,
-    this.handleProfile,
   });
 
   @override
@@ -21,25 +23,62 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future refreshExpenses(BuildContext context) async {
+      Provider.of<ExpenseProvider>(context, listen: false).refreshExpenses();
+    }
+
+    void populateExpense() async {
+      ExpenseService service = await _expenseService;
+      service
+          .populateExpense(count: 1)
+          .then((value) => refreshExpenses(context));
+    }
+
+    navigateToScreen() {
+      NavigationHelper.navigateToScreen(context, const SettingsScreen());
+    }
+
+    void handleProfile() => {
+          showDialog(
+            context: context,
+            builder: (context) => const MessageDialog(
+              title: 'To be added',
+              message: 'Profile will be added soon',
+            ),
+          ),
+        };
+
     return AppBar(
       centerTitle: true,
-      title: Text(title, textScaleFactor: 0.9),
+      title: Text(title, textScaleFactor: .8),
       backgroundColor: Colors.black,
       actions: [
         if (DebugHelper.isDebugMode)
           IconButton(
-            icon: Icon(Icons.add, size: 20, color: Theme.of(context).colorScheme.inversePrimary,),
+            icon: Icon(
+              Icons.add,
+              size: 20,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
             tooltip: "Add random expense",
             onPressed: populateExpense,
           ),
         if (DebugHelper.isDebugMode)
           IconButton(
-            icon: Icon(Icons.add_to_home_screen_outlined, size: 20, color: Theme.of(context).colorScheme.inversePrimary,),
+            icon: Icon(
+              Icons.add_to_home_screen_outlined,
+              size: 20,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
             tooltip: "Navigate to screen",
             onPressed: navigateToScreen,
           ),
         IconButton(
-          icon: Icon(Icons.person, size: 20, color: Theme.of(context).colorScheme.inversePrimary,),
+          icon: Icon(
+            Icons.person,
+            size: 20,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
           tooltip: "Profile",
           onPressed: handleProfile,
         ),
