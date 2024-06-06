e87b80c783b8650d37ef88d2aa377fb058273894
sunnydodti <sunnydodti@gmail.com>
2024-06-07 03:36:38 +0530

    add Themes

--- expense_tracker/lib/data/constants/theme_constants.dart
+++ expense_tracker/lib/data/constants/theme_constants.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-final ThemeData lightTheme = ThemeData(colorScheme: const ColorScheme.light());
+final ThemeData redTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.red));
 
-final ThemeData darkTheme = ThemeData(colorScheme: const ColorScheme.dark());
+final ThemeData orangeTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange.shade900));
+
+final ThemeData pinkTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink));
+
+final ThemeData purpleTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple));
+
+final ThemeData greenTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.green));
+
+final ThemeData lightGreenTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen));
+
+final ThemeData tealTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal.shade900));
+
+final ThemeData cyanTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan));
+
+final ThemeData blueTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue));
+
+final ThemeData indigoTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo));
+
+final ThemeData greyTheme =
+    ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey));
 
 final ThemeData blackTheme = ThemeData(
-  colorScheme: const ColorScheme.dark(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.black,
     primary: Colors.black,
-    secondary: Colors.white,
+    brightness: Brightness.dark,
+    secondary: Colors.black87,
+    tertiary: Colors.black54,
+    background: Colors.black,
   ),
 );
+
+// final ThemeData blackTheme =
+//     ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.black));
 
 final ThemeData customTheme = ThemeData(
   colorScheme: const ColorScheme.light(
