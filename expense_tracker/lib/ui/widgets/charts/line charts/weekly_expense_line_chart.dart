6c27c4c92152583c8fd4b7f954c9d5c396ffa581
sunnydodti <sunnydodti@gmail.com>
2024-08-25 21:37:05 +0530

    use provider for bar chart

--- expense_tracker/lib/ui/widgets/charts/line charts/weekly_expense_line_chart.dart
+++ expense_tracker/lib/ui/widgets/charts/line charts/weekly_expense_line_chart.dart
@@ -1,13 +1,14 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../data/constants/chart_constants.dart';
-import '../../../../data/helpers/color_helper.dart';
 import '../../../../models/chart_data.dart';
 import '../../../../models/chart_record.dart';
 import '../../../../models/enums/chart_range.dart';
+import '../../../../providers/ChartDataProvider.dart';
 import '../chart_options.dart';
+import '../chart_widgets.dart';
 
 class WeeklyExpenseLineChart extends StatefulWidget {
   final ChartData chartData;
@@ -25,19 +26,9 @@ class WeeklyExpenseLineChart extends StatefulWidget {
 }
 
 class _WeeklyExpenseLineChartState extends State<WeeklyExpenseLineChart> {
-  late int currentWeek;
-  int selectedWeek = 0;
-
-  late DateTime startDate;
-  late DateTime endDate;
-
-  bool splitLineChart = false;
-  LineChartType lineChartType = LineChartType.total;
-
   @override
   void initState() {
     super.initState();
-    currentWeek = ChartService.currentWeek;
   }
 
   @override
@@ -52,128 +43,52 @@ class _WeeklyExpenseLineChartState extends State<WeeklyExpenseLineChart> {
     );
   }
 
-  Container buildChartOptions() {
-    Map<String, DateTime> dates = ChartService.getWeekStartAndEnd(
-        selectedWeek == 0 ? currentWeek : selectedWeek);
-    return Container(
-      color: ColorHelper.getTileColor(Theme.of(context)),
-      padding: const EdgeInsets.only(top: 5),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                  textScaleFactor: .9,
-                  DateFormat('d MMM y').format(dates["start"]!)),
-              const Text(textScaleFactor: .9, "-"),
-              Text(
-                  textScaleFactor: .9,
-                  DateFormat('d MMM y').format(dates["end"]!)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                      onPressed: decrementWeek,
-                      icon: const Icon(Icons.chevron_left)),
-                  Text(
-                      textScaleFactor: .9,
-                      "Week ${selectedWeek == 0 ? currentWeek : selectedWeek}"),
-                  IconButton(
-                      onPressed: incrementWeek,
-                      icon: const Icon(Icons.chevron_right)),
-                ],
+  Consumer<ChartDataProvider> _buildWeeklyLineChart() {
+    return Consumer<ChartDataProvider>(
+      builder: (context, provider, child) {
+        Map<int, ChartRecord> dailySum = _getDailySumForWeek(provider);
+        List<LineChartBarData> lineBars = (provider.splitChart)
+            ? _buildLineBarsForSplit(dailySum)
+            : _buildLineBarsForTotal(dailySum);
+        return Container(
+          padding:
+              const EdgeInsets.only(top: 20, bottom: 5, left: 10, right: 10),
+          margin: const EdgeInsets.all(1),
+          child: LineChart(
+            LineChartData(
+              lineBarsData: lineBars,
+              gridData: FlGridData(show: false),
+              borderData: FlBorderData(show: false),
+              titlesData: FlTitlesData(
+                show: true,
+                rightTitles:
+                    AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                topTitles:
+                    AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (value, meta) =>
+                          ChartWidgets.getTitles(context, value, meta),
+                      reservedSize: 35,
+                      interval: 1),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 40,
+                      getTitlesWidget: (value, meta) =>
+                          ChartWidgets.leftTitleWidgets(value, meta)),
+                ),
               ),
-              Row(
-                children: [
-                  const Text(textScaleFactor: .9, "Split"),
-                  Checkbox(
-                      value: splitLineChart, onChanged: toggleLineChartType)
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void toggleLineChartType(value) {
-    LineChartType chartType = LineChartType.total;
-    if (value!) chartType = LineChartType.split;
-    setState(() {
-      splitLineChart = value!;
-      lineChartType = chartType;
-    });
-  }
-
-  void decrementWeek() {
-    int week = selectedWeek == 0 ? currentWeek - 1 : selectedWeek - 1;
-    if (week < 1) week = 52;
-    setState(() {
-      selectedWeek = week;
-    });
-  }
-
-  void incrementWeek() {
-    int week = selectedWeek == 0 ? currentWeek + 1 : selectedWeek + 1;
-    if (week > 52) week = 1;
-    setState(() {
-      selectedWeek = week;
-    });
-  }
-
-  Container _buildWeeklyLineChart() {
-    Map<int, ChartRecord> dailySum = _getDailySumForWeek();
-    List<LineChartBarData> lineBars = (lineChartType == LineChartType.split)
-        ? _buildLineBarsForSplit(dailySum)
-        : _buildLineBarsForTotal(dailySum);
-
-    return Container(
-      padding: const EdgeInsets.only(top: 20, bottom: 5, left: 10, right: 10),
-      margin: const EdgeInsets.all(1),
-      child: LineChart(
-        LineChartData(
-          lineBarsData: lineBars,
-          gridData: FlGridData(show: false),
-          borderData: FlBorderData(show: false),
-          titlesData: FlTitlesData(
-            show: true,
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: getTitles,
-                  reservedSize: 35,
-                  interval: 1),
+              lineTouchData: buildLineTouchData(),
             ),
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                reservedSize: 40,
-                getTitlesWidget: (value, meta) => leftTitleWidgets(value, meta),
-              ),
-            ),
+            swapAnimationCurve: Curves.linear,
+            swapAnimationDuration: const Duration(milliseconds: 250),
           ),
-          lineTouchData: buildLineTouchData(),
-        ),
-        swapAnimationCurve: Curves.linear,
-        swapAnimationDuration: const Duration(milliseconds: 250),
-      ),
+        );
+      },
     );
-  }
-
-  Widget leftTitleWidgets(double value, TitleMeta meta) {
-    String text = meta.formattedValue;
-    if (value % meta.appliedInterval != 0) text = "";
-    return Text(text, textScaleFactor: .85, textAlign: TextAlign.center);
   }
 
   LineTouchData buildLineTouchData() {
@@ -205,49 +120,9 @@ class _WeeklyExpenseLineChartState extends State<WeeklyExpenseLineChart> {
     );
   }
 
-  Map<int, ChartRecord> _getDailySumForWeek() {
-    return widget.chartData
-        .calculateDailySumForWeekLine(lineChartType, week: selectedWeek);
-  }
-
-  Widget getTitles(double value, TitleMeta meta) {
-    TextStyle style = TextStyle(
-      color: ColorHelper.getIconColor(Theme.of(context)),
-      fontWeight: FontWeight.bold,
-      fontSize: 14,
-    );
-    Widget text;
-    switch (value.toInt()) {
-      case 1:
-        text = Text('M', style: style);
-        break;
-      case 2:
-        text = Text('T', style: style);
-        break;
-      case 3:
-        text = Text('W', style: style);
-        break;
-      case 4:
-        text = Text('T', style: style);
-        break;
-      case 5:
-        text = Text('F', style: style);
-        break;
-      case 6:
-        text = Text('S', style: style);
-        break;
-      case 7:
-        text = Text('S', style: style);
-        break;
-      default:
-        text = Text('', style: style);
-        break;
-    }
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      space: 16,
-      child: text,
-    );
+  Map<int, ChartRecord> _getDailySumForWeek(ChartDataProvider provider) {
+    return widget.chartData.calculateDailySumForWeek(provider.splitChart,
+        week: provider.selectedWeek);
   }
 
   List<LineChartBarData> _buildLineBarsForTotal(
