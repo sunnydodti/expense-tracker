7a09b287d906ad36fe2cca9a7dddc55413cb26c8
sunnydodti <sunnydodti@gmail.com>
2024-09-23 02:36:30 +0530

    use adaptive bakground color for date picker

--- expense_tracker/lib/ui/dialogs/common/date_picker_dialog.dart
+++ expense_tracker/lib/ui/dialogs/common/date_picker_dialog.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/helpers/color_helper.dart';
+
 class DateTimePickerDialog {
   static Future<DateTime?> datePicker(BuildContext context) {
+    DateTime now = DateTime.now();
+    ThemeData theme = Theme.of(context);
+    ColorScheme? colorScheme = (theme.brightness == Brightness.light)
+        ? null
+        : ColorScheme(
+            brightness: theme.brightness,
+            primary: theme.primaryColor,
+            onPrimary: Colors.white,
+            secondary: theme.colorScheme.secondary,
+            onSecondary: theme.colorScheme.onSecondary,
+            error: theme.colorScheme.error,
+            onError: theme.colorScheme.onError,
+            background: theme.colorScheme.background,
+            onBackground: Colors.white,
+            surface: theme.colorScheme.surface,
+            onSurface: Colors.white,
+          );
+
+    TextButtonThemeData? textTheme = (theme.brightness == Brightness.light)
+        ? null
+        : TextButtonThemeData(
+            style: TextButton.styleFrom(foregroundColor: Colors.white));
+
     return showDatePicker(
         context: context,
-        initialDate: DateTime.now(),
+        initialDate: now,
         firstDate: DateTime(2000),
-        lastDate: DateTime(2101));
+        lastDate: DateTime(now.year + 100),
+        builder: (BuildContext context, Widget? child) {
+          return Theme(
+              data: Theme.of(context).copyWith(
+                colorScheme: colorScheme,
+                textButtonTheme: textTheme,
+                dialogBackgroundColor: ColorHelper.getTileColor(theme),
+              ),
+              child: child!);
+        });
   }
 }
