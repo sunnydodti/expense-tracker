623da51e8b404eceb12886eb523170573554a782
sunnydodti <sunnydodti@gmail.com>
2025-04-19 00:44:56 +0530

    update drawer & summary

--- expense_tracker/lib/ui/drawer/home_drawer.dart
+++ expense_tracker/lib/ui/drawer/home_drawer.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../data/helpers/color_helper.dart';
 import '../../data/helpers/navigation_helper.dart';
 import '../screens/category_screen.dart';
+import '../screens/charts_screen.dart';
 import '../screens/profile_screen.dart';
 import '../screens/settings/settings_screen.dart';
 import '../screens/tag_screen.dart';
@@ -45,24 +46,34 @@ class HomeDrawerState extends State<HomeDrawer> {
           ),
           if (NavigationHelper.isLargeScreen(context))
             ListTile(
+              leading: const Icon(Icons.home_outlined),
               title: const Text('Home'),
               onTap: () => _navigateToHomeScreen(context),
             ),
           ListTile(
+            leading: const Icon(Icons.category_outlined),
             title: const Text('Categories'),
             onTap: () => _navigateToCategoryScreen(context),
           ),
           ListTile(
+            leading: const Icon(Icons.label_outline),
             title: const Text('Tags'),
             onTap: () => _navigateToTagScreen(context),
           ),
           ListTile(
             title: const Text('Settings'),
+            leading: const Icon(Icons.settings_outlined),
             onTap: () => _navigateToSettingsScreen(context),
           ),
           ListTile(
+            leading: const Icon(Icons.person_outline),
             title: const Text('Profiles'),
             onTap: () => _navigateToProfileScreen(context),
+          ),
+          ListTile(
+            leading: const Icon(Icons.pie_chart_outline),
+            title: const Text('Charts'),
+            onTap: () => _navigateToChartsScreen(context),
           ),
         ],
       ),
@@ -83,4 +94,8 @@ class HomeDrawerState extends State<HomeDrawer> {
 
   void _navigateToProfileScreen(BuildContext context) =>
       NavigationHelper.navigateToScreen(context, const ProfileScreen());
+
+  void _navigateToChartsScreen(BuildContext context) =>
+      NavigationHelper.navigateToScreen(
+          context, const ChartsScreen(refreshData: true));
 }
