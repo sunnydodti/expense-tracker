9f136b468a91a9941a62316133e9dc23118a38c9
sunnydodti <sunnydodti.dev@gmail.com>
2025-11-27 13:48:01 +0530

    refactor - performance improvements for animations

--- expense_tracker/lib/ui/animations/blur_screen.dart
+++ expense_tracker/lib/ui/animations/blur_screen.dart
@@ -8,69 +8,77 @@ class BlurScreen extends StatefulWidget {
   final Color color;
   final double colorOpacity;
 
-  const BlurScreen(
-      {super.key,
-      this.animate = true,
-      this.onTap,
-      this.color = Colors.blue,
-      this.colorOpacity = .01});
+  const BlurScreen({
+    super.key,
+    this.animate = true,
+    this.onTap,
+    this.color = Colors.blue,
+    this.colorOpacity = 0.01,
+  });
 
   @override
   State<BlurScreen> createState() => _BlurScreenState();
 }
 
-class _BlurScreenState extends State<BlurScreen> {
-  late double blurSigmaX;
-  late double blurSigmaY;
-
-  double colorOpacity = 0;
+class _BlurScreenState extends State<BlurScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _blurAnimation;
+  late Animation<double> _colorOpacityAnimation;
 
   @override
   void initState() {
     super.initState();
-    blurSigmaX = widget.animate ? 0 : 2;
-    blurSigmaY = widget.animate ? 0 : 2;
+    initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void initialize() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _blurAnimation = Tween<double>(begin: 0.0, end: 2.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _colorOpacityAnimation =
+        Tween<double>(begin: 0.0, end: widget.colorOpacity).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    if (widget.animate) {
+      _controller.forward();
+    } else {
+      _controller.value = 1.0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.animate) animateBlur();
-    animateColor();
     return GestureDetector(
       onTap: widget.onTap,
-      child: Container(
-        color: widget.color.withOpacity(colorOpacity),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(
-            sigmaX: blurSigmaX,
-            sigmaY: blurSigmaY,
-          ),
-          child: Container(),
-        ),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Container(
+            color: widget.color.withValues(alpha: _colorOpacityAnimation.value),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: _blurAnimation.value,
+                sigmaY: _blurAnimation.value,
+              ),
+              child: const SizedBox.expand(),
+            ),
+          );
+        },
       ),
     );
-  }
-
-  void animateBlur() async {
-    while (blurSigmaX < 2) {
-      await Future.delayed(const Duration(milliseconds: 50));
-      if (mounted) {
-        setState(() {
-          blurSigmaX += .01;
-          blurSigmaY += .01;
-        });
-      }
-    }
-  }
-
-  void animateColor() async {
-    while (colorOpacity < widget.colorOpacity) {
-      await Future.delayed(const Duration(milliseconds: 50));
-      if (mounted) {
-        setState(() {
-          colorOpacity += .001;
-        });
-      }
-    }
   }
 }
--- expense_tracker/lib/ui/animations/blur_widget.dart
+++ expense_tracker/lib/ui/animations/blur_widget.dart
@@ -14,47 +14,58 @@ class BlurWidget extends StatefulWidget {
   State<BlurWidget> createState() => _BlurWidgetState();
 }
 
-class _BlurWidgetState extends State<BlurWidget> {
-  late double blurSigmaX;
-  late double blurSigmaY;
+class _BlurWidgetState extends State<BlurWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _blurAnimation;
 
   @override
   void initState() {
     super.initState();
-    blurSigmaX = widget.animate ? 0 : widget.blur;
-    blurSigmaY = widget.animate ? 0 : widget.blur;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _blurAnimation = Tween<double>(begin: 0.0, end: widget.blur).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    if (widget.animate) {
+      _controller.forward();
+    } else {
+      _controller.value = 1.0;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (widget.animate) animateBlur();
     return ClipRect(
       child: Stack(
         children: [
           Center(child: widget.widget),
-          BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: blurSigmaX,
-              sigmaY: blurSigmaY,
+          Positioned.fill(
+            child: AnimatedBuilder(
+              animation: _blurAnimation,
+              builder: (context, child) {
+                return BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: _blurAnimation.value,
+                    sigmaY: _blurAnimation.value,
+                  ),
+                  child: const SizedBox.expand(),
+                );
+              },
             ),
-            child: Container(
-              color: Colors.transparent,
-            ),
-          )
+          ),
         ],
       ),
     );
-  }
-
-  void animateBlur() async {
-    while (blurSigmaX < widget.blur) {
-      await Future.delayed(const Duration(milliseconds: 50));
-      if (mounted) {
-        setState(() {
-          blurSigmaX += 3;
-          blurSigmaY += 3;
-        });
-      }
-    }
   }
 }
