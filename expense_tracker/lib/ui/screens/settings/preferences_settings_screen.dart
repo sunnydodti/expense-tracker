9895dd00edfa23a18a66110797bc2d6e89a8c8a2
sunnydodti <sunnydodti.dev@gmail.com>
2025-11-29 19:26:49 +0530

    preformance improvements, standardization & refactor - settings & preferences screen

--- expense_tracker/lib/ui/screens/settings/preferences_settings_screen.dart
+++ expense_tracker/lib/ui/screens/settings/preferences_settings_screen.dart
@@ -4,35 +4,16 @@ import '../../../data/helpers/color_helper.dart';
 import '../../widgets/common/screen_app_bar.dart';
 import '../../widgets/preferences/preferences_list.dart';
 
-class PreferencesSettingsScreen extends StatefulWidget {
+class PreferencesSettingsScreen extends StatelessWidget {
   const PreferencesSettingsScreen({super.key});
 
   @override
-  State<PreferencesSettingsScreen> createState() =>
-      _PreferencesSettingsScreenState();
-}
-
-class _PreferencesSettingsScreenState extends State<PreferencesSettingsScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return buildPreferencesWidget();
-  }
-
-  Scaffold buildPreferencesWidget() {
     return Scaffold(
       backgroundColor: ColorHelper.getBackgroundColor(Theme.of(context)),
       appBar: const ScreenAppBar(title: 'Preferences'),
       body: const Column(
-        children: [
-          Expanded(
-            child: PreferencesList(),
-          )
-        ],
+        children: [Expanded(child: PreferencesList())],
       ),
     );
   }
--- expense_tracker/lib/ui/screens/settings/settings_screen.dart
+++ expense_tracker/lib/ui/screens/settings/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/constants/ui_constants.dart';
 import '../../../data/helpers/color_helper.dart';
 import '../../../data/helpers/navigation_helper.dart';
 import '../../dialogs/delete_all_dialog.dart';
@@ -10,26 +11,8 @@ import '../../widgets/expandable_list_tile.dart';
 import '../../widgets/theme_selector.dart';
 import 'preferences_settings_screen.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
-
-  @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _getDefaults();
-  }
-
-  void _getDefaults() async {}
-
-  Future<void> _refreshSettings(BuildContext context) async {
-    // final provider = Provider.of<CategoryProvider>(context, listen: false);
-    // provider.refreshCategories();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,33 +22,28 @@ class _SettingsScreenState extends State<SettingsScreen> {
       body: Column(
         children: [
           Expanded(
-            child: FutureBuilder<void>(
-              future: _refreshSettings(context),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return ListView(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    children: [
-                      const ExpandableListTile(
-                          title: 'Import', content: ImportForm()),
-                      const ExpandableListTile(
-                          title: 'Export', content: ExportForm()),
-                      const DeleteAllDialog(),
-                      ListTile(
-                        title: const Text('Preferences'),
-                        onTap: () => NavigationHelper.navigateToScreen(
-                            context, const PreferencesSettingsScreen()),
-                      ),
-                      const ThemeSelector()
-                      // buildThemeDropdown(context)
-                    ],
-                  );
-                }
-              },
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: uiPadding),
+              children: [
+                const ExpandableListTile(
+                  title: 'Import',
+                  content: ImportForm(),
+                ),
+                const ExpandableListTile(
+                  title: 'Export',
+                  content: ExportForm(),
+                ),
+                const DeleteAllDialog(),
+                ListTile(
+                  title: const Text('Preferences'),
+                  onTap: () => NavigationHelper.navigateToScreen(
+                    context,
+                    const PreferencesSettingsScreen(),
+                  ),
+                ),
+                const ThemeSelector()
+                // buildThemeDropdown(context)
+              ],
             ),
           )
         ],
