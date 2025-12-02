e64dc41f4c6701aacb26ecf6fe2545c7534f70e0
sunnydodti <sunnydodti.dev@gmail.com>
2025-12-02 12:42:21 +0530

    release 0.0.11

--- expense_tracker/lib/data/constants/db_constants.dart
+++ expense_tracker/lib/data/constants/db_constants.dart
@@ -89,7 +89,7 @@ class DatabaseVersionConstants {
   final String databaseVersionKey = "database_version";
   final int databaseVersion = 3;
   final String appVersionKey = "app_version";
-  final String appVersion = "0.0.10";
+  final String appVersion = "0.0.11";
   final String createdAt = "created_at";
 }
 
